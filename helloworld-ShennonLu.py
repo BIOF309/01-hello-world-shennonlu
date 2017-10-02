4cd2bfe65145b75e46259321b9610c7cfd532326
shennonlu <31867653+shennonlu@users.noreply.github.com>
2017-10-01 21:46:39 -0400

    Add files via upload

---
+++ helloworld-ShennonLu.py
@@ -0,0 +1,32 @@
+
+# coding: utf-8
+
+# In[1]:
+
+print("Hello, world")
+
+
+# In[2]:
+
+print("We are going to exercise today!")
+
+
+# In[3]:
+
+asktemp = input("What's the temperature in degrees Fahrenheit, now?")
+
+
+# In[4]:
+
+if int(asktemp) >= 85:
+    print ("Wow, that's pretty hot! You should go swimming.")
+if int(asktemp) <85 and int(asktemp) >= 40:
+    print ("What a great day, you should go running.")
+if int(asktemp) < 40:
+    print ("Wow, it's pretty cold. Perfect for skiing!")
+
+
+# In[ ]:
+
+
+
